b79af865e8ab638cef488db13acc06397279d6bc
1juanherrera <juansebas751@gmail.com>
2023-08-11 13:55:26 -0500

    Switch built

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from "react-redux"
+import { useSelector } from "react-redux"
 import { ButtonNav, Preloader, Hero, AboutMe, Portfolio, Contact, Switch } from "./components"
 import { NavLink } from "./components/NavLink"
 
