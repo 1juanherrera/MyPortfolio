664c983ca6b7be9822f5228a7a7aacdfd11656aa
1juanherrera <juansebas751@gmail.com>
2023-09-18 17:24:33 -0500

    portfolio built

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,6 +1,10 @@
 import { motion } from "framer-motion"
+import { useSelector } from "react-redux"
 
 export const Preloader = () => {
+
+    const { isLanguage } = useSelector( state => state.language )
+
     return (
         <>
             <motion.div
@@ -14,21 +18,21 @@ export const Preloader = () => {
                         initial={{ y: '100%', opacity: 1 }}
                         transition={{ duration: 1 }}
                         whileInView={{ opacity: 1, y: '0' }}
-                        className="preloader__text">I Craft</motion.span>
+                        className="preloader__text">{ isLanguage ? 'Construyo' : 'I Craft' }</motion.span>
                     </h1>
                     <h1 className="preloader__hide">
                         <motion.span 
                         initial={{ y: '100%', opacity: 1 }}
                         transition={{ duration: 1.2 }}
                         whileInView={{ opacity: 1, y: '0' }}
-                        className="preloader__text">Websites That</motion.span>
+                        className="preloader__text">{ isLanguage ? 'Sitios web que' : 'Websites That' }</motion.span>
                     </h1>
                     <h1 className="preloader__hide">
                         <motion.span 
                         initial={{ y: '100%', opacity: 1 }}
                         transition={{ duration: 1.4 }}
                         whileInView={{ opacity: 1, y: '0' }}
-                        className="preloader__text">You'd Love Using!</motion.span>
+                        className="preloader__text">{ isLanguage ? 'Te encantaría usar' : "You'd Love Using!" }</motion.span>
                     </h1>
                 </div>
             </motion.div>
@@ -37,9 +41,20 @@ export const Preloader = () => {
             transition={{ duration: 3, delay: 1 }}
             whileInView={{ opacity: 1, y: '-100%' }}
             className="preloader__slider">
-                <h4>Welcome to</h4>
-                <h1>Juan Herrera</h1>
-                <h4>portfolio</h4>
+                {
+                    isLanguage ?
+                    <>
+                    <h4>Bienvenido al</h4>
+                    <h2>Portafolio de</h2>
+                    <h1>Juan Herrera</h1>
+                    </>
+                    :
+                    <>
+                    <h4>Welcome to</h4>
+                    <h1>Juan Herrera</h1>
+                    <h4>portfolio</h4>
+                    </>
+                }
             </motion.div>
       </>
     )
--- src/store/slices/language/languageSlice.js
+++ src/store/slices/language/languageSlice.js
@@ -1,13 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const isLanguage = localStorage.getItem('language') !== null 
+? JSON.parse(localStorage.getItem('language')) : true;
+
 export const languageSlice = createSlice({
    name: 'language',
    initialState: {
-       isLanguage: false
+       isLanguage: isLanguage
    },
    reducers: {
         changeLanguage: ( state ) => {
             state.isLanguage = !state.isLanguage;
+            localStorage.setItem('language', JSON.stringify(state.isLanguage));
         },
     }
 });
